56b7201ea7c1cb7e4da3adfcd3b1208311bf9adb
Pornticha <68775548+Porntichanice@users.noreply.github.com>
2021-08-28 22:05:43 +0700

    wk3

--- test2.js
+++ test2.js
@@ -1,1 +1,4 @@
-let x = 10;
+let year = 2001;
+let findYear = ['Monkey','Rooster','Dog','Pig','Rat','Ox','Tiger','Rabbit','Dragon','Shake','Horse','Sheep'];
+let a = year % 12;
+console.log(findYear[a]);
