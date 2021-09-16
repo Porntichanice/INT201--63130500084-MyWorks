3fdcaa7ba9d9d70dac600b9b7d714b36cb23c79b
Pornticha <68775548+Porntichanice@users.noreply.github.com>
2021-09-16 17:17:23 +0700

    new1

--- wk6.js
+++ wk6.js
@@ -127,3 +127,18 @@ console.log(arr2);
 let arr3 = 'hello world';
 let arr4 = [...arr3];
 console.log(arr4.length);
+
+//----------------5.00
+//include object variable  in array with spread operator
+let per1 = [{ id: 1, name: 'Adam' }];
+let arr1 = [...per1];
+
+arr1[0].name = 'Joe';
+console.log(per1);
+
+//include primitive variable in array with spread operator
+let num1 = [10];
+let arr2 = [...num1];
+
+arr2[0] = 20;
+console.log(num1);
